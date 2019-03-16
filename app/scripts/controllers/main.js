733c19c1544a2371eae2f8f1da3b42cbdbc82d7c
kalkut0001 <42506144+kalkut0001@users.noreply.github.com>
2019-03-16 18:21:34 +0530

    Fresh install

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name shineEbsApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the shineEbsApp
+ */
+angular.module('shineEbsApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
