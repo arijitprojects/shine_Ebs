b9924fb798f2b684bf9b5d58e86da74d30a96e0f
kalkut0001 <42506144+kalkut0001@users.noreply.github.com>
2019-03-31 22:10:09 +0530

    update master

---
+++ app/scripts/authentication.service.js
@@ -0,0 +1,45 @@
+
+(function () {
+    'use strict';
+
+    angular
+        .module('onlineMockTestApp')
+        .factory('AuthenticationService', AuthenticationService);
+
+    AuthenticationService.$inject = ['$http', '$cookies', '$rootScope', '$timeout'];
+    function AuthenticationService($http, $cookies, $rootScope, $timeout) {
+        var service = {};
+
+        service.SetCredentials = SetCredentials;
+        service.ClearCredentials = ClearCredentials;
+
+        return service;
+
+      
+        function SetCredentials(authdata) {
+
+            $rootScope.globals = {
+                currentUser: {
+                    authdata: authdata
+                }
+            };
+
+            // set default auth header for http requests
+            $http.defaults.headers.common['Authorization'] = 'Basic ' + authdata;
+
+            // store user details in globals cookie that keeps user logged in for 1 week (or until they logout)
+            var cookieExp = new Date();
+            cookieExp.setDate(cookieExp.getDate() + 7);
+            $cookies.putObject('globals', $rootScope.globals, { expires: cookieExp });
+        }
+
+        function ClearCredentials() {
+            $rootScope.globals = {};
+            $cookies.remove('globals');
+            $http.defaults.headers.common.Authorization = 'Basic';
+        }
+    }
+
+  
+
+})();
--- app/views/login/login.controller.js
+++ app/views/login/login.controller.js
@@ -3,11 +3,34 @@
 
     angular
         .module('shineEbsApp')
-        .controller('loginController', ['$scope', '$http', 'restApiFactory','$parse','$location',
-        function loginController($scope, $http, restApiFactory,$parse,$location){
+        .controller('loginController', ['$scope', '$http', 'restApiFactory','$parse','$location','AuthenticationService',
+        function loginController($scope, $http, restApiFactory,$parse,$location,AuthenticationService){
+            $scope.loginForm = {};
         
-        
-        
+            $scope.submitForm = function(){
+                if(!$scope.loginForm.username && !$scope.loginForm.password){
+
+                }
+                else{
+                    restApiFactory("POST", "http://localhost:8989/A_ezi/organization/userLogin.php",
+                     JSON.stringify({username:$scope.loginForm.username,
+                    password:$scope.loginForm.password})).
+                     post(function(responseData){
+                            if(responseData.status == "200"){
+                                if(responseData.result.error==false){
+                                    AuthenticationService.SetCredentials(responseData); 
+                                    $location.path("/");  
+                                }
+                               
+                            }
+                            }, function (responseData){
+                            console.log("Error :" , responseData);
+                            });
+
+                }
+
+
+            }
         
         }])
 
