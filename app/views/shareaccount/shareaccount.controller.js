813575348e65c58c7e92f35990a6cbd5f8ffd4bb
kalkut0001 <42506144+kalkut0001@users.noreply.github.com>
2019-05-25 18:46:42 +0530

    update master

--- app/views/shareaccount/shareaccount.controller.js
+++ app/views/shareaccount/shareaccount.controller.js
@@ -6,8 +6,37 @@
         .controller('shareaccountController', ['$scope', '$http', 'restApiFactory','$parse','$location',
         function shareaccountController($scope, $http, restApiFactory,$parse,$location){
         
-        
-        
+            $scope.share = {};
+         //Share Class
+         restApiFactory("GET", "http://localhost:8989/A_ezi/share/shareClass.php", {}
+         ).post(function (responseData) {
+             if (responseData.status == "200") {
+
+                 $scope.shareClasses = responseData.shareClass;
+                 console.log($scope.shareClasses);
+             }
+         }, function (responseData) {
+             console.log("Error :", responseData);
+         });
+
+         $scope.GetSelectedshare = function () {
+             console.log("$scopeshare.shareClassIdId : ", $scope.share.shareClassIdId);
+         }
+         //Submit cif
+         $scope.submitCIFForm = function () {
+            restApiFactory("POST", "http://localhost:8989/A_ezi/customermapping/fetchWithCif.php",
+                JSON.stringify({ CIF_NO: $scope.share.Membership_No_CIF_No})).post(function (responseData) {
+                    if (responseData.status == "200") {
+                        //  $scope.gp.gpId = "339";
+
+                        $scope.share.cif_details = responseData.cifinfo;
+                        console.log($scope.share.cif_details);
+                    }
+                }, function (responseData) {
+                    console.log("Error :", responseData);
+                });
+        }
+
         
         }])
 
--- app/views/sharemaster/sharemaster.controller.js
+++ app/views/sharemaster/sharemaster.controller.js
@@ -3,12 +3,30 @@
 
     angular
         .module('shineEbsApp')
-        .controller('sharemasterController', ['$scope', '$http', 'restApiFactory','$parse','$location',
-        function sharemasterController($scope, $http, restApiFactory,$parse,$location){
+        .controller('sharemasterController', ['$rootScope','$scope', '$http', 'restApiFactory','$parse','$location',
+        function sharemasterController($rootScope,$scope, $http, restApiFactory,$parse,$location){
         
-        
-        
-        
+            $scope.shareMaster = {};
+           
+            $scope.submitForm = function () {
+            console.log("has value :  : "+JSON.stringify(
+                { shareMaster:$scope.shareMaster,
+                    PUBLIC_KEY:$rootScope.globals.currentUser.authdata.authToken }));
+
+                    restApiFactory("POST", 
+                    "http://localhost:8989/A_ezi/share/shareInsert.php", 
+                    JSON.stringify(
+                        { shareMaster:$scope.shareMaster,
+                            PUBLIC_KEY:$rootScope.globals.currentUser.authdata.authToken })).post(function(responseData){
+                        if(responseData.status == "200"){
+                            alert('share Master Created, click to Home page');
+                            $location.path("/");
+                        }
+                        }, function (responseData){
+                        console.log("Error :" , responseData);
+                        });
+            }
+       
         }])
 
   
