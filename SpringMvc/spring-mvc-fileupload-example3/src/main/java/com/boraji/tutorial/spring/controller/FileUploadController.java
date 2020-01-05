6af9412edb55f91ad6627a7dbb18d6b1be308529
kalkut0001 <42506144+kalkut0001@users.noreply.github.com>
2020-01-05 17:36:10 +0530

    file upload
    file

---
+++ SpringMvc/spring-mvc-fileupload-example3/src/main/java/com/boraji/tutorial/spring/controller/FileUploadController.java
@@ -0,0 +1,45 @@
+package com.boraji.tutorial.spring.controller;
+
+import java.io.BufferedOutputStream;
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
+
+/**
+ * @author imssbora
+ */
+@Controller
+public class FileUploadController {
+
+   @GetMapping("/")
+   public String fileUploadForm(Model model) {
+      return "fileUploadForm";
+   }
+
+   // Handling file upload request
+   @PostMapping("/fileUpload")
+   public ResponseEntity<Object> fileUpload(@RequestParam("file") MultipartFile file,
+		   @RequestParam("text") String text )
+         throws IOException {
+
+	   String st = text;
+	   String fileName = file.getOriginalFilename();
+      // Save file on system
+      if (!file.getOriginalFilename().isEmpty()) {
+    	  String fileName1 = file.getOriginalFilename();
+      }else{
+         return new ResponseEntity<>("Invalid file.",HttpStatus.BAD_REQUEST);
+      }
+      
+      return new ResponseEntity<>("File Uploaded Successfully.",HttpStatus.OK);
+   }
+}
